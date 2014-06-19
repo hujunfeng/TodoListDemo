62582d60b66c8c332da5c5a620c77f208929bea4
Hu Junfeng <junfeng.hu@2359media.com>
2014-06-19 19:16:32 +0800

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Hu Junfeng on 19/6/14.
+//  Copyright (c) 2014 Hu Junfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
